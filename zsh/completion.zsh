28dfb49069c7e532df3dad034a2cd54e0b35f356
CarlosGolvano <carlosgoolvin@gmail.com>
2025-07-18 04:24:51 +0200

    Update

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,106 +1,143 @@
-  # ____ ___  __  __ ____  _     _____ _____ ___ ___  _   _ 
-#  / ___/ _ \|  \/  |  _ \| |   | ____|_   _|_ _/ _ \| \ | |
-# | |  | | | | |\/| | |_) | |   |  _|   | |  | | | | |  \| |
-# | |__| |_| | |  | |  __/| |___| |___  | |  | | |_| | |\  |
-#  \____\___/|_|  |_|_|   |_____|_____| |_| |___\___/|_| \_|
- #
+# ====================================
+# ZSH COMPLETION CONFIGURATION
+# ====================================
 
 # +---------+
-# | General |
+# | BASIC   |
 # +---------+
 
-# source ./gambit.zsh
-
-# Load more completions
+# Load additional completions (external plugins)
 fpath=($DOTFILES/zsh/plugins/zsh-completions/src $fpath)
 
-# Should be called before compinit
-zmodload zsh/complist
+# Initialize completion system
+autoload -U compinit
+compinit
 
-# Use hjlk in menu selection (during completion)
-# Doesn't work well with interactive mode
-bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'k' vi-up-line-or-history
-bindkey -M menuselect 'j' vi-down-line-or-history
-bindkey -M menuselect 'l' vi-forward-char
-
-bindkey -M menuselect '^xg' clear-screen
-bindkey -M menuselect '^xi' vi-insert                      # Insert
-bindkey -M menuselect '^xh' accept-and-hold                # Hold
-bindkey -M menuselect '^xn' accept-and-infer-next-history  # Next
-bindkey -M menuselect '^xu' undo                           # Undo
-
-autoload -U compinit; compinit
-_comp_options+=(globdots) # With hidden files
-
-# Only work with the Zsh function vman
-# See $DOTFILES/zsh/scripts.zsh
-compdef vman="man"
+# Include hidden files in completion
+_comp_options+=(globdots)
 
 # +---------+
-# | Options |
+# | OPTIONS |
 # +---------+
 
-# setopt GLOB_COMPLETE      # Show autocompletion menu with globs
-setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
-setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
-setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word
+setopt ALWAYS_TO_END        # Move cursor to end after completion
 
 # +---------+
-# | zstyles |
+# | BEHAVIOR |
 # +---------+
 
-# Ztyle pattern
-# :completion:<function>:<completer>:<command>:<argument>:<tag>
+# NO interactive menu - classic behavior
+zstyle ':completion:*' menu no
 
-# Define completers
-zstyle ':completion:*' completer _extensions _complete _approximate
+# Basic completers
+zstyle ':completion:*' completer _complete _approximate
 
-# Use cache for commands using cache
+# Use cache for slow commands
 zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-# Complete the alias when _expand_alias is used as a function
-zstyle ':completion:*' complete true
+zstyle ':completion:*' cache-path "$HOME/.zsh/cache"
 
-zle -C alias-expension complete-word _generic
-bindkey '^Xa' alias-expension
-zstyle ':completion:alias-expension:*' completer _expand_alias
+# +---------+
+# | MATCHING |
+# +---------+
 
-# Use cache for commands which use it
+# Smart matching rules
+zstyle ':completion:*' matcher-list \
+    '' \
+    'm:{a-z}={A-Z}' \
+    'm:{a-zA-Z}={A-Za-z}' \
+    'r:|[._-]=* r:|=* l:|=*'
 
-# Allow you to select in a menu
-zstyle ':completion:*' menu select
+# +---------+
+# | COLORS   |
+# +---------+
 
-# Autocomplete options for cd instead of directory stack
-zstyle ':completion:*' complete-options true
+# Use ls colors for files
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
-zstyle ':completion:*' file-sort modification
+# Informative messages
+zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:warnings' format '%F{red}-- No matches found --%f'
 
+# +---------+
+# | GROUPING |
+# +---------+
 
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-# zstyle ':completion:*:default' list-prompt '%S%M matches%s'
-# Colors for files and directory
-zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
-
-# Only display some tags for the command cd
-zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
-# zstyle ':completion:*:complete:git:argument-1:' tag-order !aliases
-
-# Required for completion to be in good groups (named after the tags)
+# Group matches by category
 zstyle ':completion:*' group-name ''
 
+# Sort files by modification time (newest first)
+zstyle ':completion:*' file-sort modification
+
+# Order for different completion types
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
 zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
 
-# See ZSHCOMPWID "completion matching control"
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+# +----------+
+# | COMMANDS |
+# +----------+
 
-zstyle ':completion:*' keep-prefix true
+# Kill command completion
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
+zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 
-zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+# SSH/SCP completion from known hosts
+zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
 
-## For kubernetes
-source $DOTFILES/zsh/plugins/kubectl-completion/_kubectl
-zstyle ':completion:*:*:kubectl:*' list-grouped false
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+        adm amanda apache at avahi avahi-autoipd beaglidx bin cacti canna \
+        clamav daemon dbus distcache dnsmasq dovecot fax ftp games gdm \
+        gkrellmd gopher hacluster haldaemon halt hsqldb ident junkbust kdm \
+        ldap lp mail mailman mailnull man messagebus mldonkey mysql nagios \
+        named netdump news nfsnobody nobody nscd ntp nut nx obsrun openvpn \
+        operator pcap polkitd postfix postgres privoxy pulse pvm quagga radvd \
+        rpc rpcuser rpm rtkit scard shutdown squid sshd statd svn sync tftp \
+        usbmux uucp vcsa wwwrun xfs '_*'
+
+# +---------------+
+# | APPROXIMATION |
+# +---------------+
+
+# Allow up to 2 errors for approximate completion
+zstyle ':completion:*:approximate:*' max-errors 2 numeric
+
+# Format for corrections
+zstyle ':completion:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+
+# +----------+
+# | GIT/VCS  |
+# +----------+
+
+# Git completion optimizations
+zstyle ':completion:*:*:git:*' tag-order 'common-commands'
+zstyle ':completion:*:*:git*:*' group-order 'main commands' 'alias commands' 'external commands'
+
+# +----------------+
+# | DOCKER/KUBECTL |
+# +----------------+
+
+# Docker completion
+zstyle ':completion:*:*:docker:*' option-stacking yes
+zstyle ':completion:*:*:docker-*:*' option-stacking yes
+
+# +------------------+
+# | FILE EXCLUSIONS  |
+# +------------------+
+
+# Ignore certain file types
+zstyle ':completion:*:*:(vim|nvim|nano):*:*files' ignored-patterns '*~' '*.o' '*.pyc' '*.class'
+zstyle ':completion:*:*:rm:*:*files' ignored-patterns
+
+# +-------------+
+# | PERFORMANCE |
+# +-------------+
+
+# Faster completion for large directories
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache yes
+zstyle ':completion:*' cache-path ~/.zsh/cache
+
+# Only complete .zsh files for source command
+zstyle ':completion:*:*:source:*' file-patterns '*.zsh:zsh-files' '*:all-files'
