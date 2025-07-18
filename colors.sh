46229c78a845dc2bab3f60b6f4d12ceceeee4cdd
CarlosGolvano <carlosgoolvin@gmail.com>
2025-07-18 12:55:04 +0200

    Update

--- colors.sh
+++ colors.sh
@@ -1,19 +1,20 @@
  #!/usr/bin/env bash
 
+# Pastel color palette
 default="\033[39m"
 black="\033[30m"
-red="\033[0;31m"
-green="\033[32m"
-yellow="\033[33m"
-blue="\033[34m"
-magenta="\033[35m"
-cyan="\033[36m"
+red="\033[38;5;217m"          # Pastel red/pink
+green="\033[38;5;157m"        # Pastel mint green
+yellow="\033[38;5;229m"       # Pastel cream yellow
+blue="\033[38;5;153m"         # Pastel sky blue
+magenta="\033[38;5;189m"      # Pastel lavender
+cyan="\033[38;5;159m"         # Pastel aqua
 light_gray="\033[37m"
 dark_gray="\033[90m"
-light_red="\033[91m"
-light_green="\033[92m"
-light_yellow="\033[93m"
-light_blue="\033[94m"
-light_magenta="\033[95m"
-light_cyan="\033[96m"
+light_red="\033[38;5;224m"    # Very light pastel pink
+light_green="\033[38;5;194m"  # Very light pastel green
+light_yellow="\033[38;5;230m" # Very light pastel yellow
+light_blue="\033[38;5;195m"   # Very light pastel blue
+light_magenta="\033[38;5;225m" # Very light pastel purple
+light_cyan="\033[38;5;195m"   # Very light pastel cyan
 white="\033[97m"
---
+++ install/install-kitty.sh
@@ -0,0 +1,4 @@
+mkdir -p "$XDG_CONFIG_HOME/kitty"
+
+ln -sf "$DOTFILES/kitty/kitty.conf" "$XDG_CONFIG_HOME/kitty/kitty.conf"
+ln -sf "$DOTFILES/kitty/color.ini" "$XDG_CONFIG_HOME/kitty/color.ini"
